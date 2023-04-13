5a99ccb2831d2f216e93afeb296eaa856b32a7bf
geothomspon <geo.thom18@gmail.com>
2023-04-13 11:03:09 -0500

    added more comments

--- nvim/lua/geothompson/set.lua
+++ nvim/lua/geothompson/set.lua
@@ -1,5 +1,8 @@
+ --line numbers
 vim.opt.nu = true
+ --relative line numbers
 vim.opt.relativenumber = true
+
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
@@ -19,25 +22,33 @@ vim.opt.backup   = false
 vim.opt.undodir  = "/home/eeby/.vim/undodir"
 vim.opt.undofile  = true
 
+--stops every serach result from being highlighted
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 
 vim.opt.termguicolors = true
 
+
+-- keeps cursor in middle of page
 vim.opt.scrolloff = 8
+
+
+-- adds column for vim diagnostics/etc
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 
+-- improves preformance
 vim.opt.updatetime = 50
 vim.opt.lazyredraw  = true
 
 vim.opt.smartcase = true
 
+
+-- adds visual column
 vim.opt.colorcolumn = "80"
 
-vim.opt.nu = true
 vim.opt.conceallevel= 1
 
 
