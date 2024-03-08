0c3dbde0ef55ab71d46a5b6037c7f30dc9cd6845
George Thompson <Thom7918@umn.edu>
2024-03-08 16:05:55 -0600

    updated nvim config dir

--- nvim/after/plugin/leetcode.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd("nnoremap <leader>li :LeetCodeSignIn<cr>")
-vim.cmd("nnoremap <leader>ll :LeetCodeList<cr>")
-vim.cmd("nnoremap <leader>lt :LeetCodeTest<cr>")
-vim.cmd("nnoremap <leader>ls :LeetCodeSubmit<cr>")
-
-vim.cmd("let g:leetcode_browser='chrome'")
-vim.cmd("let g:leetcode_solution_filetype='python3'" )
